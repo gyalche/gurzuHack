1a589a7c620616e45a20983571b55550d11eb2d0
Dawa Sherpa <fupulamu01@gmail.com>
2024-08-02 11:01:27 +0545

    nextjs setup initila commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,8 @@
+import LoginComp from "@/components/LoginComp";
+import React from "react";
+
+const Login = () => {
+  return <LoginComp />;
+};
+
+export default Login;
---
+++ src/components/LoginComp.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+type Props = {};
+
+const LoginComp = (props: Props) => {
+  return (
+    <Box className="w-screen border h-screen flex items-center justify-center">
+      <Box></Box>
+    </Box>
+  );
+};
+
+export default LoginComp;
